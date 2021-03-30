e322b766f5d6a1ada8ca0efab520d93799bc5d37
Yvan Sabay <sabayyvan2018@gmail.com>
2021-03-30 08:18:06 +0800

    Added clear function and changed swatches to text-advanced

--- src/views/entertainment/Draw.vue
+++ src/views/entertainment/Draw.vue
@@ -2,10 +2,12 @@
   <div>
     <div class="container-fluid" style="height: 100vh">
       <canvas class="drawcanvas" ref="drawcanvas" id="drawcanvas" @mousedown="startPosition" @mouseup="finishedPosition" @mousemove="draw"></canvas>
-      <div class="color-container align-items-center justify-content-center">
-        <div class="d-block">
+      <div class="color-container ">
+        <div class="col">
           <p class="text-white">Color</p>
-          <v-swatches v-model="color"></v-swatches>
+          <v-swatches v-model="color" shapes="circles" swatches="text-advanced" ></v-swatches>
+          <p class="text-white  mt-3">Clear</p>
+          <div class="clear" @click.prevent="clearCanvas"></div>
         </div>
       </div>
     </div>
@@ -21,6 +23,12 @@ export default {
       painting: false,
       ctx: '',
       color: 'white',
+      swatches: [
+        ['#F64272', '#F6648B', '#F493A7', '#F891A6'],
+        ['#8b5aff', '#a27bff', '#b99cff', '#d0bdff'],
+        ['#51e5db', '#74ebe3', '#96f0ea', '#b9f5f1'],
+        ['#ffa51a', '#ffb748', '#ffc976', '#ffdba3']
+      ]
     }
   },
   mounted() {
@@ -51,6 +59,9 @@ export default {
     finishedPosition(){
       this.painting = false;
       this.ctx.beginPath();
+    },
+    clearCanvas(){
+      this.ctx.clearRect(0, 0, this.ctx.width, this.ctx.height);
     }
   }
 }
@@ -58,13 +69,23 @@ export default {
 <style>
 .color-container {
   width: 200px;
-  height: 100vh;
+  height: calc(100vh - 10%);
   position: fixed;
   right: 0;
   top: 0;
   display: flex;
-  justify-content: center;
   align-items: center;
 }
 
+.clear {
+  height: 42px;
+  width: 42px;
+  position: relative;
+  clip-path: polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%);
+  background-color: white;
+}
+
+.clear:hover {
+  cursor: pointer;
+}
 </style>
